9b83da1d35b78007d5a805d4d11fcdfb2906989d
N'GUESSAN <debadychatue@gmail.com>
2025-02-18 14:13:19 +0000

    ajout des prérequis pour l'inscription et connexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity'; // Assurez-vous du bon chemin
+import { User } from './entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // Change si tu es sur un autre serveur
+      host: 'localhost',
       // host: 'localhost', // Change si tu es sur un autre serveur
-      port: 5432, // Par défaut, PostgreSQL utilise ce port
-      username: 'postgres', // Ton utilisateur PostgreSQL
-      password: 'NGUESSAN', // Mets ton vrai mot de passe
-      database: 'hostolink_bd', // Ta base de données
+      port: 5432, 
+      username: 'postgres', 
+      password: 'NGUESSAN', 
+      database: 'hostolink_bd', 
       autoLoadEntities: true,
       entities: [User], // On ajoute l'entité `User`
       synchronize: true, // Mettre à `false` en production
     }),
-    UsersModule, // Ajoute le module des utilisateurs
+    UsersModule,
+    AuthModule, // Ajoute le module des utilisateurs
   ],
 })
 export class AppModule {}
