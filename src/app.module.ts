43efd6ca8669ca64fa5eca5e3e5eb0792c5c6c01
N'GUESSAN <debadychatue@gmail.com>
2025-04-09 14:44:09 +0200

    dev payement user a es par email ou numero de telephone

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,143 +1,143 @@
 // // // // ---------------------LOCAL ---------------------
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-
-// ✅ Utilisateurs & Authentification
-import { UserModule } from './utilisateur/user.module';
-import { AuthModule } from './auth/auth.module';
-
-// ✅ OTP & Notifications
-
-// ✅ Images & Établissements de Santé
-import { ImageModule } from './image/image.module';
-import { ListeNumeroEtablissementSanteModule } from './liste_etablissement/liste_numero_etablissement_sante.module';
-import { CloudinaryModule } from './upload/cloudinary.module';
-import { AdministrateurModule } from './administrateur/administrateur.module';
-import { AnnonceModule } from './annonce/annonce.module';
-
-import { ThematiqueDiscussionModule } from './thematique_discussion/thematique_discussion.module';
-import { FirebaseModule } from './thematique_discussion/firebase/firebase.module';
-
-
-// ✅ transaction interne
-import { TransactionInterneModule } from './transaction-interne/transaction-interne.module';
-import { TransactionFraisModule } from './transaction-frais/transaction-frais.module';
-import { UserEtablissementSante } from './user_etablissement_sante/entities/user-etablissement-sante.entity';
-import { UserEtablissementSanteModule } from './user_etablissement_sante/user-etablissement-sante.module';
-import { QrDynamiqueModule } from './qr-dynamique_user_es/qr-dynamique.module';
-import { PaiementModule } from './paiement_user_a_es/paiement.module';
-
-@Module({
-  
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: Number(process.env.DATABASE_PORT) || 5432,
-      username: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'NGUESSAN',
-      database: process.env.DATABASE_NAME || 'hostolink_bd',
-      autoLoadEntities: true,
-      synchronize: false, 
-      logging:true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  }),
-  AuthModule,
-  CloudinaryModule,
-  // FirebaseModule,
-
-  AdministrateurModule,
-
-  // UserModule,
-  // ImageModule,
-  // ListeNumeroEtablissementSanteModule,
-  // AnnonceModule,
-  // ThematiqueDiscussionModule,
-  // TransactionFraisModule,
-  // TransactionInterneModule,
-  
-  UserEtablissementSanteModule,
-  QrDynamiqueModule,
-  PaiementModule
-
-  ],
-
-})
-export class AppModule {}
-
-
-
-// ----------en ligne -----------------
-// -
 // import { Module } from '@nestjs/common';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 // import { ConfigModule } from '@nestjs/config';
-// import { AuthModule } from './auth/auth.module';
-// import { ImageModule } from './image/image.module';
+
+// // ✅ Utilisateurs & Authentification
 // import { UserModule } from './utilisateur/user.module';
-// import { GestionUtilisateurModule } from './administrateur/Gest_utilisateurs/gestion_utilisateur.module';
-// import { AdministrateurModule } from './administrateur/administrateur.module';
+// import { AuthModule } from './auth/auth.module';
+
+// // ✅ OTP & Notifications
+
+// // ✅ Images & Établissements de Santé
+// import { ImageModule } from './image/image.module';
 // import { ListeNumeroEtablissementSanteModule } from './liste_etablissement/liste_numero_etablissement_sante.module';
 // import { CloudinaryModule } from './upload/cloudinary.module';
+// import { AdministrateurModule } from './administrateur/administrateur.module';
 // import { AnnonceModule } from './annonce/annonce.module';
+
 // import { ThematiqueDiscussionModule } from './thematique_discussion/thematique_discussion.module';
 // import { FirebaseModule } from './thematique_discussion/firebase/firebase.module';
-// import { TransactionFraisModule } from './transaction-frais/transaction-frais.module';
+
+
+// // ✅ transaction interne
 // import { TransactionInterneModule } from './transaction-interne/transaction-interne.module';
-// import { EtablissementSanteModule } from './localisation_etablissement_sante/etablissement_sante.module';
+// import { TransactionFraisModule } from './transaction-frais/transaction-frais.module';
 // import { UserEtablissementSante } from './user_etablissement_sante/entities/user-etablissement-sante.entity';
 // import { UserEtablissementSanteModule } from './user_etablissement_sante/user-etablissement-sante.module';
 // import { QrDynamiqueModule } from './qr-dynamique_user_es/qr-dynamique.module';
 // import { PaiementModule } from './paiement_user_a_es/paiement.module';
 
-
 // @Module({
+  
 //   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true,
-//     }),
+//     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+
 //     TypeOrmModule.forRoot({
 //       type: 'postgres',
-//       host: process.env.DB_HOST,
-//       port: Number(process.env.DB_PORT),
-//       username: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
+      
+//       host: process.env.DATABASE_HOST || 'localhost',
+//       port: Number(process.env.DATABASE_PORT) || 5432,
+//       username: process.env.DATABASE_USER || 'postgres',
+//       password: process.env.DATABASE_PASSWORD || 'NGUESSAN',
+//       database: process.env.DATABASE_NAME || 'hostolink_bd',
 //       autoLoadEntities: true,
-//       synchronize: false,
-//       migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//       migrationsRun: true,
-//       logging:false,
-//       //  process.env.NODE_ENV !== 'production',
-//       extra: process.env.DB_SSL === 'true'
-//         ? { ssl: { rejectUnauthorized: false } }
-//         : undefined,
-//     }),
-//     UserModule, 
-//     AuthModule, 
-//     ImageModule, 
-//     AdministrateurModule,
-//     GestionUtilisateurModule,
-    
-//     ListeNumeroEtablissementSanteModule,
-//     CloudinaryModule,
-//     AnnonceModule,
-//     ThematiqueDiscussionModule,
-//     FirebaseModule,
-//     TransactionFraisModule,
-//     TransactionInterneModule,
-//     EtablissementSanteModule, 
-//     UserEtablissementSante,
-//     UserEtablissementSanteModule,
-//     QrDynamiqueModule,
-//     PaiementModule,
-//     AdministrateurModule,
+//       synchronize: false, 
+//       logging:true,
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   }),
+//   AuthModule,
+//   CloudinaryModule,
+//   // FirebaseModule,
+
+//   AdministrateurModule,
+
+//   // UserModule,
+//   // ImageModule,
+//   // ListeNumeroEtablissementSanteModule,
+//   // AnnonceModule,
+//   // ThematiqueDiscussionModule,
+//   // TransactionFraisModule,
+//   // TransactionInterneModule,
+  
+//   UserEtablissementSanteModule,
+//   QrDynamiqueModule,
+//   PaiementModule
 
 //   ],
+
 // })
 // export class AppModule {}
-// console.log('📌 Connexion à PostgreSQL avec URL :', process.env.DB_HOST);
+
+
+
+// ----------en ligne -----------------
+// -
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ImageModule } from './image/image.module';
+import { UserModule } from './utilisateur/user.module';
+import { GestionUtilisateurModule } from './administrateur/Gest_utilisateurs/gestion_utilisateur.module';
+import { AdministrateurModule } from './administrateur/administrateur.module';
+import { ListeNumeroEtablissementSanteModule } from './liste_etablissement/liste_numero_etablissement_sante.module';
+import { CloudinaryModule } from './upload/cloudinary.module';
+import { AnnonceModule } from './annonce/annonce.module';
+import { ThematiqueDiscussionModule } from './thematique_discussion/thematique_discussion.module';
+import { FirebaseModule } from './thematique_discussion/firebase/firebase.module';
+import { TransactionFraisModule } from './transaction-frais/transaction-frais.module';
+import { TransactionInterneModule } from './transaction-interne/transaction-interne.module';
+import { EtablissementSanteModule } from './localisation_etablissement_sante/etablissement_sante.module';
+import { UserEtablissementSante } from './user_etablissement_sante/entities/user-etablissement-sante.entity';
+import { UserEtablissementSanteModule } from './user_etablissement_sante/user-etablissement-sante.module';
+import { QrDynamiqueModule } from './qr-dynamique_user_es/qr-dynamique.module';
+import { PaiementModule } from './paiement_user_a_es/paiement.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      migrationsRun: true,
+      logging:false,
+      //  process.env.NODE_ENV !== 'production',
+      extra: process.env.DB_SSL === 'true'
+        ? { ssl: { rejectUnauthorized: false } }
+        : undefined,
+    }),
+    UserModule, 
+    AuthModule, 
+    ImageModule, 
+    AdministrateurModule,
+    GestionUtilisateurModule,
+    
+    ListeNumeroEtablissementSanteModule,
+    CloudinaryModule,
+    AnnonceModule,
+    ThematiqueDiscussionModule,
+    FirebaseModule,
+    TransactionFraisModule,
+    TransactionInterneModule,
+    EtablissementSanteModule, 
+    UserEtablissementSante,
+    UserEtablissementSanteModule,
+    QrDynamiqueModule,
+    PaiementModule,
+    AdministrateurModule,
+
+  ],
+})
+export class AppModule {}
+console.log('📌 Connexion à PostgreSQL avec URL :', process.env.DB_HOST);
