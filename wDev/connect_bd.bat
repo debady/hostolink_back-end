2e1dcc32f0a6fa9b9c6ab86f1bf3ae5d11d2799e
some joseph <somejoseph2@gmail.com>
2025-05-02 15:40:40 +0000

    amend

--- wDev/connect_bd.bat
+++ wDev/connect_bd.bat
@@ -1,7 +1,7 @@
 psql "host=aws-0-eu-west-3.pooler.supabase.com port=5432 dbname=postgres user=postgres.skwupmsitzsxukbmnkwv password=6640ywfeiQqiBMM5 sslmode=require"
 
 AVEC DOCKER:
-    docker exec -it NOM8DUnom_du_conteneur bash
+    docker exec -it nom_du_conteneur bash
 
     psql "host=aws-0-eu-west-3.pooler.supabase.com port=5432 dbname=postgres user=postgres.skwupmsitzsxukbmnkwv password=6640ywfeiQqiBMM5 sslmode=require"
 
