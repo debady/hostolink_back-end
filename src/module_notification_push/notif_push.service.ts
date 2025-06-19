6ebfb74ba490f3698390ccf9271e8c54d4c159fa
debady <debadychatue@gmail.com>
2025-06-19 17:07:18 +0200

    creer un nouveau projet firebase dedier a hostopay

--- src/module_notification_push/notif_push.service.ts
+++ src/module_notification_push/notif_push.service.ts
@@ -8,7 +8,7 @@ export class NotifPushService {
     if (!admin.apps.length) {
       const serviceAccountPath = path.join(
         process.cwd(),
-        'hostolink-notification-firebase-adminsdk-fbsvc-832dc603f9.json'
+        'hostopay-notif-firebase-adminsdk-fbsvc-cf9e79b521.json'
       );
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccountPath),
