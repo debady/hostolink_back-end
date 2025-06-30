501318c935adad5c1b9bdb1080df21e21dcf7658
Explora-studio <startup.explora.studio@gmail.com>
2025-07-01 01:31:52 +0200

    correction de nom de entiyt d'enregistremlent d etransaction

--- src/wave-checkout/wave-checkout.module.ts
+++ src/wave-checkout/wave-checkout.module.ts
@@ -7,7 +7,7 @@ import { User } from 'src/utilisateur/entities/user.entity';
 import { Compte } from 'src/compte/entitie/compte.entity';
 import { TransactionsFrais } from './entities/frais_depot_wave.entity';
 import { TransactionExterne } from './entities/transaction_externe.entity';
-import { HistoriqueTransactions } from './entities/historique_transactions.wave.entity.';
+import { HistoriqueTransactions } from './entities/historique_transactions.wave.entity';
 import { NotificationTransaction } from './entities/notification_transaction.entity';
 
 @Module({
--- src/wave-checkout/wave-checkout.service.ts
+++ src/wave-checkout/wave-checkout.service.ts
@@ -12,7 +12,7 @@ import { ConfigService } from '@nestjs/config';
 import { CreateWaveSessionDto } from './dto/create-wave-session.dto';
 import { TransactionsFrais } from './entities/frais_depot_wave.entity';
 import { TransactionExterne } from './entities/transaction_externe.entity';
-import { HistoriqueTransactions } from './entities/historique_transactions.wave.entity.';
+import { HistoriqueTransactions } from './entities/historique_transactions.wave.entity';
 import { NotificationTransaction } from './entities/notification_transaction.entity';
 import { Compte } from 'src/compte/entitie/compte.entity';
 import { User } from 'src/utilisateur/entities/user.entity';
